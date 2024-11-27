892db65e7962a575ecfdf79533ea6653a13c63ff
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-11-27 14:53:04 +0300

    update padding

--- src/client/components/chat-widget.tsx
+++ src/client/components/chat-widget.tsx
@@ -211,7 +211,7 @@ export const ChatWidget = () => {
               </div>
             </CardHeader>
             <CardContent>
-              <div className="h-[350px] pr-4 overflow-auto" ref={scrollAreaRef}>
+              <div className="h-[350px] px-4 overflow-auto" ref={scrollAreaRef}>
                 {!hasMessages && !isLoading && (
                   <div className="text-center opacity-50 text-sm absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 dark:text-yellow-100">
                     Chat with the AI assistant
