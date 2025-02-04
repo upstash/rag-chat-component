ee6de1e083bf369f337b3f3f57bc803e6ab957f7
Fahreddin Özcan <88107904+fahreddinozcan@users.noreply.github.com>
2025-02-04 22:17:33 +0300

    Responsive widget (#5)
    * ui: responsive widget
    
    * 0.2.2-canary.0
    
    * 0.2.2-canary.1
    
    * fix height
    
    * 0.2.2-canary.2
    
    * version
    
    * remove comment

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,18 @@ import type { Config } from "tailwindcss";
 const config = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    animation: {
-      "accordion-down": "accordion-down 0.2s ease-out",
-      "accordion-up": "accordion-up 0.2s ease-out",
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
